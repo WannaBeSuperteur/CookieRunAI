5599bdb92a0c8d44afbdd6325d57c8a3033b6158
WannaBeSuperteur <32893014+WannaBeSuperteur@users.noreply.github.com>
2020-04-13 17:28:16 +0900

    augment image based on info in file

--- augmentImage.py
+++ augmentImage.py
@@ -6,6 +6,7 @@ from PIL import Image
 
 ## save cropped image with specified pixels for top, bottom, left and right
 # location    : location where train and test images exist
+# file_list   : list of image files to crop
 # width       : width of each element in the array
 # height      : height of each element in the array
 # top         : crop rows at the top N pixels
@@ -14,9 +15,8 @@ from PIL import Image
 # right       : crop columns at the right N pixels
 # minWidth    : minimum width of cropped image
 # minHeight   : minimum height of cropped image
-def cropImgs(location, top, bottom, left, right, minWidth, minHeight):
-    file_list = os.listdir(location)
-
+# i           : changing value to prevent duplicated file name
+def cropImgs(location, file_list, top, bottom, left, right, minWidth, minHeight, i):
     for file in range(len(file_list)): # for each image
         im = Image.open(location + file_list[file])
         width = im.size[0] # width of image
@@ -29,7 +29,27 @@ def cropImgs(location, top, bottom, left, right, minWidth, minHeight):
         im = im.crop((left, top, width-right, height-bottom))
 
         # save: XXX.yyy -> XXX(number).yyy (eg: 1_01.png -> 1_0149.png)
-        im.save(location + file_list[file].split('.')[0] + str(file) + '.' + file_list[file].split('.')[1])
+        im.save(location + file_list[file].split('.')[0] + str(file + i * len(file_list)) +
+                '.' + file_list[file].split('.')[1])
+
+## read file and crop images using the content of file
+# fileName    : file name
+def cropImgsUsingFile(fileName):
+    
+    f = open(fileName, 'r')
+    fl = f.readlines()
+
+    # 1st line indicates location where train and test images exist
+    location = fl[0].split('\n')[0]
+    file_list = os.listdir(location) # file list
+    f.close()
+
+    # 2nd ~ last lines indicate [top, bottom, left, right, minWidth, minHeight]
+    # eg: '4 4 4 4 16 24' -> top=4, bottom=4, left=4, right=4, minWidth=16, minHeight=24
+    for i in range(1, len(fl)):
+        args = fl[i].split('\n')[0].split(' ')
+        cropImgs(location, file_list, int(args[0]), int(args[1]), int(args[2]),
+                 int(args[3]), int(args[4]), int(args[5]), i-1)
 
 if __name__ == '__main__':
-    cropImgs('images/test/', 4, 4, 4, 4, 16, 24)
+    cropImgsUsingFile('augment.txt')
